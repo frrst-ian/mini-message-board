6b4237033422ad2a3797bbb19d2175511c5d2223
frrst-ian <frrstxd@proton.me>
2025-02-13 12:53:07 +0800

    fix: create a proper Procfile

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const newRouter = require('./routes/newRouter')
 
 app.use(express.urlencoded({extended:true}));
 
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
@@ -14,8 +24,13 @@ app.use('/', indexRouter);
 app.use('/new', newRouter);
 
 const PORT = process.env.PORT || 3001;
+const HOST = '0.0.0.0'
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT http://localhost:${PORT}`);
+    console.log(`Listening on PORT ${HOST}:${PORT}`);
 });
 
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+});
